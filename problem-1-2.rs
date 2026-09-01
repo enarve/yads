9a2d69541cdb8de7a72eecb5d744fa13e4e74a29
Nikita <80875524+enarve@users.noreply.github.com>
2026-09-01 15:29:50 +0200

    Update problem-1-2.rs

--- problem-1-2.rs
+++ problem-1-2.rs
@@ -17,56 +17,30 @@ fn main() {
         .map(|s| s.parse::<i64>().expect("Error reading value."))
         .collect();
 
-    // Looking for values
-    let counterparts: Vec<i64> = get_counterparts(values, n);
+    // Finding problem solution
+    let result: Vec<i64> = get_result(values, n);
 
     // Output
-    let counterparts_strings: Vec<String> = counterparts.iter().map(|i| i.to_string()).collect();
-    let formatted_string = format!("{}", counterparts_strings.join(" "));
-    write_output(formatted_string);
+    let result_strings: Vec<String> = result.iter().map(|i| i.to_string()).collect();
+    let formatted_res_string = format!("{}", result_strings.join(" "));
+    write_output(formatted_res_string);
 }
 
 // For each value get appropriate pair: smallest value with bigger index
-fn get_counterparts(values: Vec<i64>, length: usize) -> Vec<i64> {
+fn get_result(values: Vec<i64>, length: usize) -> Vec<i64> {
     assert!(values.len() == length);
-    let mut counterparts: Vec<i64> = vec![];
-    let mut stack: Stack<i64> = Stack::new();
+    let mut result: Vec<i64> = vec![];
     for (i, a) in values.iter().enumerate() {
-        let mut min_value: i64 = -1;
+        let mut min_index: i64 = -1;
         for (j, b) in values.iter().enumerate() {
-            if i < j && b > a && (*b < min_value || min_value == -1)  {
-                min_value = *b;
+            if i < j && b > a  {
+                min_index = (j + 1) as i64;
+                break;
             }
         }
-        counterparts.push(min_value);
+        result.push(min_index);
     }
-    counterparts
-}
-
-// fn get_counterparts(values: Vec<i64>, length: usize) -> Vec<i64> {
-//     let mut counterparts: Vec<i64> = vec![];
-//     let mut stack: Stack<i64> = Stack::new();
-
-//     if let Some(last_in_stack) = stack.last_element() {
-//         if value > last_in_stack {
-//             stack.push(*value);
-//         }
-//     } else {
-//         stack.push(*value);
-//     }
-    
-//     counterparts
-// }
-
-// Stack imitation
-struct Stack<T> { items: Vec<T> }
-
-impl<T> Stack<T> {
-    fn new() -> Self { Self { items: vec![] } }
-    fn push(&mut self, item: T) { self.items.push(item); }
-    fn pop(&mut self) -> Option<T> { self.items.pop() }
-    fn last_element(&self) -> Option<&T> { self.items.last() }
-    fn length(&self) -> usize { self.items.len() }
+    result
 }
 
 fn read_input() -> String {
